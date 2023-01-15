01d7b6d7fced671a449132ba1705162592143e12
William Fournier <wfournier2710@gmail.com>
2023-01-15 17:38:25 -0500

    Delete goat image + fix last commit

---
+++ JojofiedMonk/SoundChoice.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JojofiedMonk
+{
+    public enum SoundChoice
+    {
+        ORA,
+        MUDA
+    }
+}
